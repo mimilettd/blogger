3c52cdaa9b05a5994ef1afe100fffd491d9d8d3f
mimilettd <mimilettd@gmail.com>
2017-08-25 20:21:50 -0600

    refactor test validations for Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  subject {
+    described_class.new(title: "Learn Ruby the Hard Way",
+                        body: "In seven weeks, learn how to code like a real programmer!")
+          }
   it "is valid with valid attributes" do
-    article = Article.new(title: "Learn Ruby the Hard Way", body: "In seven weeks, learn how to code like a real programmer!")
-    expect(article).to be_valid
+    expect(subject).to be_valid
   end
   it "is not valid without a title" do
-    article = Article.new(body: "Learn Ruby the Hard Way")
-    expect(article).to_not be_valid
+    subject.title = nil
+    expect(subject).to_not be_valid
   end
   it "is not valid without a body" do
-    article = Article.new(title: "Head First")
-    expect(article).to_not be_valid
+    subject.body = nil
+    expect(subject).to_not be_valid
   end
 end
