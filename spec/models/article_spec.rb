44a98a3eff69dd1061dc1aa4e2db882d4794300e
mimilettd <mimilettd@gmail.com>
2017-08-25 20:14:18 -0600

    create validation tests for Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    article = Article.new(title: "Learn Ruby the Hard Way", body: "In seven weeks, learn how to code like a real programmer!")
+    expect(article).to be_valid
+  end
+  it "is not valid without a title" do
+    article = Article.new(body: "Learn Ruby the Hard Way")
+    expect(article).to_not be_valid
+  end
+  it "is not valid without a body" do
+    article = Article.new(title: "Head First")
+    expect(article).to_not be_valid
+  end
 end
